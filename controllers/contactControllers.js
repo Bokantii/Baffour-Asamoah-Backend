14a86cb931eea54c83584aced7aa6e554ed9a2e0
USER <user@USERs-MacBook-Pro.local>
2025-09-28 22:08:12 -0400

    Updated email

--- controllers/contactControllers.js
+++ controllers/contactControllers.js
@@ -3,15 +3,13 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const resend = new Resend(process.env.RESEND_API_KEY);
-
-
 exports.sendContactMessage = async (req, res) => {
   const { firstName, lastName, email, message } = req.body;
 
   try {
     const response = await resend.emails.send({
-      from: 'Dr. Baffour Website <onboarding@resend.dev>',  
-      to: 'teejaysoarz@gmail.com',                          
+      from: 'New Lead!!!😍😍<onboarding@resend.dev>',  
+      to: 'jtemenu@gmail.com',                          
       subject: `New message from ${firstName} ${lastName}`,
       html: `
         <p><strong>From:</strong> ${firstName} ${lastName} (${email})</p>
--- routes/contact.js
+++ routes/contact.js
@@ -1,9 +1,17 @@
+// const express = require('express');
+// const router = express.Router();
+// const { sendContactMessage } = require('../controllers/contactControllers');
+// const handlers = require('../controllers/contactControllers');
+// console.log(handlers); 
+
+
+// router.post('/', sendContactMessage);
+
+// module.exports = router;
+
 const express = require('express');
 const router = express.Router();
 const { sendContactMessage } = require('../controllers/contactControllers');
-const handlers = require('../controllers/contactControllers');
-console.log(handlers); 
-
 
 router.post('/', sendContactMessage);
 
